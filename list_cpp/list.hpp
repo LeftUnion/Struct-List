f8076dae615d9f15c86f2ddb54ad9bf42f022cc0
sergey45467321 <kruto454@gmail.com>
2022-11-16 16:06:10 +0300

    [add] 'move' function, include std::excexpion [ref] make some functions const

--- list_cpp/list.hpp
+++ list_cpp/list.hpp
@@ -2,6 +2,9 @@
 #define LIST_HPP
 
 #include <string>
+#include <exception>
+#include <stdexcept>
+
 
 template <class T>
 class Node
@@ -13,6 +16,7 @@ class Node
 template <class T>
 class list
 {
+    public:
     Node<T>* first;
     Node<T>* last;
     int count;
@@ -21,30 +25,138 @@ class list
     ~list();
     
     void pushBack(T value);
-    bool isEmpty();
+    bool isEmpty() const;
+    Node<T>* move(int index) const;
+    void insert(T value, int index);
+    void insert(T value, Node<T>* index);
+
 };
+
+template <class T>
+Node<T>* move(int index)
+{
+    
+    try
+    {
+        if(index < 0 || index >= count)
+            throw std::invalid_argument("Out of indexes!");
+
+        if(count > 0)
+        {
+            Node<T>* it = first;
+            for(int i = 0; i < index; i++)
+                it = it->next;
+
+            return it
+        }
+        
+        return nullptr;
+
+    }
+    catch(std::invalid_argument arg)
+    {
+        std::cerr <<  arg.what() << std::endl;
+    }
+}
 
 template <class T>
 void list<T>::pushBack(T value)
 {
-    Node<T>* tmp = new Node<T>;
-    tmp->value = value;
-    tmp->next = nullptr;
-
-    if(this->isEmpty())
-        first = last = tmp;
-    else
+    try
     {
-        last->next = tmp;
-        last = tmp;
+        Node<T>* tmp = new Node<T>;
+        tmp->value = value;
+        tmp->next = nullptr;
+
+        if(this->isEmpty())
+            first = last = tmp;
+        else
+        {
+            last->next = tmp;
+            last = tmp;
+        }
+        count++;
     }
-    count++;
+    catch(std::bad_alloc err)
+    {
+        std::cerr << err.what() << std::endl;
+    }
 }
 
 template <class T>
 bool list<T>::isEmpty()
 {
     return first == nullptr;
+}
+
+template <class T>
+void list<T>::insert(T value, int index)
+{
+    try
+    {
+        if ((count != 0) && ((index < 0) || (index >= count)))
+            throw std::invalid_argument("Out of indexes!");
+
+
+        // if Exists
+        if(first == nullptr)
+        {
+            pushBack(value);
+        }
+        else
+        {
+            Node<T>* tmp = new Node<T>;
+            tmp->value = value;   
+            if(index == 0)
+            {
+                tmp->next = first;
+                first = tmp;
+            }
+            else if ()
+            {
+             
+            }
+        }
+
+        count++;
+    }
+    catch(std::bad_alloc err)
+    {
+        std::cerr << err.what() << std::endl;
+    }
+    catch(std::invalid_argument arg)
+    {
+        std::cerr <<  arg.what() << std::endl;
+    }
+
+}
+
+template <class T>
+void list<T>::insert(T value, Node<T>* index)
+{
+    try
+    {
+        if ((count != 0) && ((index < 0) || (index >= count)))
+            throw std::invalid_argument("Out of indexes!");
+
+        if(this->isEmpty())
+        {
+            pushBack(value);
+        }
+        elif(inde)
+        {
+
+        }
+    }
+    catch(std::bad_alloc err)
+    {
+        std::cerr << err.what() << std::endl;
+    }
+    catch(std::invalid_argument arg)
+    {
+        std::cerr <<  arg.what() << std::endl;
+    }
+
 }
 
 template <class T>
